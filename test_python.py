5c24d55f5d118c084c9d1a596c163d640d6caee0
shaneDong <33693850+SHANExiang@users.noreply.github.com>
2020-07-22 23:05:02 +0800

    [python] 局部变量可修改全局列表变量，没加global不能重新赋值，sys.modules[__name__]使用

--- test_python.py
+++ test_python.py
@@ -1,4 +1,5 @@
 import copy
+import sys
 
 
 # 深拷贝与浅拷贝
@@ -116,7 +117,7 @@ def test_copy():
 # test_copy()
 
 
-# 闭包
+# 闭包                       **************************------------------------
 # 调用func的时候就产生了闭包（inner_func+name），这意味着当func声明周期结束的时候，name
 # 这个变量依旧存在，因为它被闭包引用了，所以不会被回收。
 # 所谓闭包---在函数内部定义一个函数，并且这个函数用到了外部函数的变量，那么讲这个函数与用到
@@ -130,7 +131,7 @@ def func(name):
 #      print('函数inner_func的内部地址为%s' % f)
 
 
-# 装饰器
+# 装饰器                      **************************------------------------
 # 1. 不带参数的装饰器
 def wrap1(func):
     def inner_func(*args, **kwargs):
@@ -168,19 +169,74 @@ def func2():
 # func2()
 
 
-# random模块使用
+# random模块使用              **************************------------------------
 import random
 # 生成一个范围内的随机数
-print(random.randrange(1, 10))   # 左开右闭,不包括10
-print(random.randrange(1, 20, 3))   # 1-20之间，步长为3的数字
-print(random.randint(1, 10))   # 1-10之间的整数，包括10
-print(random.choice('dongxiang1243'))   # 在一个字符串中间选择一个
-print(random.choice((1, 2, 34, '34')))   # 返回一个给定数据集合的随意数据
-print(random.sample('shanedfgv2335', 4))   # 返回给定数量的数据集合的列表
-print(random.sample([2, 34, '34', 3], 4))   # 返回给定数量的数据集合的列表
-l = [3, 4, 5, 6, 7]
-random.shuffle(l)
-print(l)     # 打乱数据集合
+# print(random.randrange(1, 10))   # 左开右闭,不包括10
+# print(random.randrange(1, 20, 3))   # 1-20之间，步长为3的数字
+# print(random.randint(1, 10))   # 1-10之间的整数，包括10
+# print(random.choice('dongxiang1243'))   # 在一个字符串中间选择一个
+# print(random.choice((1, 2, 34, '34')))   # 返回一个给定数据集合的随意数据
+# print(random.sample('shanedfgv2335', 4))   # 返回给定数量的数据集合的列表
+# print(random.sample([2, 34, '34', 3], 4))   # 返回给定数量的数据集合的列表
+# l = [3, 4, 5, 6, 7]
+# random.shuffle(l)
+# print(l)     # 打乱数据集合
+
+
+# 函数中修改列表以及列表中的元素 **************************------------------------
+lis = [1, 2, 3, 4, 5]
+# print(id(lis))
+
+
+def change_list1():
+    # 此lis与外边的lis作用域不一样，这是局部变量, 这个意思是将存放[1, 2, 3]这个列表的内存
+    # 地址赋给了lis，而lis这个变量已经指向了[1, 2, 3, 4, 5]这个内存地址，
+    # print(id(lis))   # 要是局部变量与全局变量名称一致时，函数中只认此局部变量
+    lis = [1, 2, 3]
+    print(id(lis))
+
+
+# change_list1()
+# print(lis)
+
+
+def change_list2():
+    global lis   # 只有在函数中利用global关键字声明后才能修改全局变量
+    print(id(lis))
+    lis = [1, 2, 3]  # 相当于此lis变量就是全局变量lis
+    print(id(lis))
+
+
+# change_list2()
+# print(lis)
+
+
+def change_list3():
+    print(id(lis))
+    lis.append(6)
+    del lis[0]
+    l = lis
+    print(l)
+
+# 函数中可以改变全局列表变量中的值，但不能修改其引用，除非加上global
+# 对于列表、字典、集合这样的数据结构以及类、对象中的元素我们都可以在函数中修改；
+# 而对于数字、字符串这样的数据类型只有在函数中利用global关键字声明后才能对全局变量进行修改！
+# change_list3()
+# print(lis)    # [2, 3, 4, 5, 6]
+
+
+# sys.modules[__name__]      **************************------------------------
+
+
+def function():
+    print('test sys.modules')
+
+
+print(sys.modules[__name__])   # 此python文件中的module 'main'
+print(function.__name__)    # 函数名
+function_obj = getattr(sys.modules[__name__], function.__name__)# 获得此函数名对象
+function_obj()
 
 
 
