855d581a67bcbe2040170e6e5d56fe9028942223
shaneDong <33693850+SHANExiang@users.noreply.github.com>
2020-08-07 23:00:51 +0800

    [python] 生成器send使用

--- iterator_and_generator/test_generator.py
+++ iterator_and_generator/test_generator.py
@@ -11,10 +11,10 @@ def func():
     print("four------------>")
     yield 4
 
-
+# 执行代码
 # 生成器就是个迭代器，有__iter__和__next__属性
-print(hasattr(func(), '__next__')) # True
-print(hasattr(func(), '__iter__')) # True
+# print(hasattr(func(), '__next__')) # True
+# print(hasattr(func(), '__iter__')) # True
 
 
 # 生成器模拟Linux下tail -f a.txt | grep 'error' | grep '404'
@@ -35,9 +35,55 @@ def grep(generator, pattern): # generator为生成器类型
         if pattern in generator:
             yield line
 
+############################################################################
 
-gen1 = grep(tail('test.txt'), 'error') # 动态跟踪文件新添加的内容,并且过滤出有patterns的行
-gen2 = grep(gen1, '404')  # gen1为生成器
+# 执行代码
+# gen1 = grep(tail('test.txt'), 'error') # 动态跟踪文件新添加的内容,并且过滤出有patterns的行
+# gen2 = grep(gen1, '404')  # gen1为生成器
 
-for x in gen2:   # 通过for循环来隐式调用__next__()方法
-    print(x)
+# for x in gen2:   # 通过for循环来隐式调用__next__()方法
+#     print(x)
+
+
+def foo():
+    while True:
+         x = yield
+         print('x==%s' % x)
+
+
+# f = foo()
+# print(next(f)) # None 程序运动到yield就卡住，等待下一个next
+# f.send('dong') # x==dong给yield发送值dong,然后这个值被赋值给了x，并且打印出来,然后继续下一次循环停在yield处
+# f.send('xiang') # x==xiang
+# print(next(f)) # None没有给x赋值，执行print语句，打印出None,继续循环停在yield处
+
+
+############################################################################
+def deco(func):
+    def wrapper():
+        res = func()
+        next(res)
+        return res
+    return wrapper
+
+
+@deco
+def func1():
+    food_list = list()
+    while True:
+        x = yield food_list
+        food_list.append(x)
+        print('food_list==%s' % food_list)
+
+
+f1 = func1()
+f1.send('水蜜桃')
+f1.send('榴莲')
+f1.send('苹果')
+
+# output
+# food_list==['水蜜桃']
+# food_list==['水蜜桃', '榴莲']
+# food_list==['水蜜桃', '榴莲', '苹果']
+
+
