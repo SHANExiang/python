4685be37f70b65279e7665e280100fc7f0a0a6e1
shaneDong <33693850+SHANExiang@users.noreply.github.com>
2020-12-13 22:46:17 +0800

    [python] leetcode+1

---
+++ test_leetcode/linkedlist/reversed_linkedlist.py
@@ -0,0 +1,26 @@
+
+
+class LinkedList(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def reverse_linkedlist1(self, head):
+        prev = None
+        cur = head
+        while not cur:
+            node = cur.next
+            cur.next = prev
+            prev = cur
+            cur = node
+        return prev
+
+    def reverse_linkedlist2(self, head):
+        if head == None || head.next == None:
+            return head
+        node = self.reverse_linkedlist2(head.next)
+        head.next.next = head
+        head.next = None
+        return node
